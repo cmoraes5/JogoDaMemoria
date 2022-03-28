28c18ac3afcba8f0e179f53b09a501e1506c04e6
cmoraes5 <moraescaio0502@gmail.com>
2022-03-28 21:56:02 +0000

    Shuffle cards

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,8 @@
+// Cards front and back
 const FRONT = "card_front";
 const BACK = "card_back";
 
+// Type of cards, with respective techs
 let techs = ['bootstrap',
     'css',
     'electron',
@@ -12,22 +14,57 @@ let techs = ['bootstrap',
     'node',
     'react'];
 
-    createCardsFromTechs(techs);
+let cards = null;
 
+// Executing the startGame
+startGame()
+
+// When game started, cards receive createCards function, then, this cards were shuffle
+function startGame(){
+    cards = createCardsFromTechs(techs);
+    shuffleCards(cards);
+    console.log(cards);
+}
+
+
+function shuffleCards(cards){
+    let currentIndex = cards.length;
+    let randomIndex = 0;
+
+    while(currentIndex !== 0){
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]];
+
+
+
+    }
+}
+
+// Executing the function for Create the cards
+createCardsFromTechs(techs);
+
+// The function who create cards techs
 function createCardsFromTechs(techs){
 
+    // Every card beggin empty
     let cards = [];
 
+    // For every var tech inside techs array, add to card the product of the function, create pair
     for(let tech of techs){
         cards.push(createPairFromTech(tech))
     }
 
-    console.log(cards.flatMap(pair => pair));
+    //Return the cards and respective pairs
     return cards.flatMap(pair => pair);
 }
 
+// Create the pair of every type of cards techs
 function createPairFromTech(tech){
 
+    // return an array with object and propieties
     return [{
         id: createIdWithTech(tech),
         icon: tech,
@@ -39,6 +76,9 @@ function createPairFromTech(tech){
     }]
 }
 
+// Create the id for every card, this way, evening the cards pair was the "same", actully it will was not
 function createIdWithTech(tech){
+
+    // Create an random number for id
     return tech + parseInt(Math.random() * 1000);
 }
